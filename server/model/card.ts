8020618dd2b109e7cca26a681ef036359eb973a5
david-ferm00 <david@ferm.co.uk>
2023-02-01 11:51:21 +0100

    Added a part of the model(cards and piles)

---
+++ server/model/card.ts
@@ -0,0 +1,105 @@
+export class Pile{
+    pile: Array<Card> = [];
+
+    constructor(empty : Boolean){
+        if(!empty){
+            let pile = this.shuffleDeck(this.generateNewPile());
+        } else {
+            let pile = [];
+        }
+    }
+
+    generateNewPile(): Array<Card> {
+        let result = [];
+        for (let i = 0; i < 5; i++) {
+            for (let j = 0; j < 13; j++) {
+                result.push(new Card(i,j));
+                if(j!=0){
+                    result.push(new Card(i,j));
+                }
+            }
+        }
+
+        for (let index = 0; index < 2; index++) {
+            for (let j = 0; j < 4; j++) {
+                result.push(new Card(4, index+13));
+            }
+            
+        }
+        return [];
+    }
+
+    shuffleDeck(deck : Array<Card>): Array<Card>{
+        let currentIndex = deck.length,  randomIndex;
+
+        while (currentIndex != 0) {
+  
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+  
+            [deck[currentIndex], deck[randomIndex]] = [
+                deck[randomIndex], deck[currentIndex]];
+        }
+  
+        return deck;
+    }
+
+    pickTopCard(): Card{
+        let card: Card = this.pile.pop()!;
+        if(card != undefined){
+            return card;
+        }
+        throw console.error("pile empty");
+        
+    }
+    
+    seeTopCard(): Card{
+        let card: Card = this.pile.pop()!;
+        if(card != undefined){
+            let returnCard = new Card(card.colour,card.value);
+            this.pile.push(card);
+            return returnCard;
+        }
+        throw console.error("pile empty");
+    }
+
+    addCard(card:Card) {
+        this.pile.push(card);
+    } 
+}
+
+export class Card {
+    colour : Colour;
+    value : Value;
+
+    constructor(colour: Colour, value: Value) {
+        this.colour = colour;
+        this.value = value;
+    }
+}
+
+enum Colour{
+    red,
+    yellow,
+    blue,
+    green,
+    none
+}
+
+enum Value{
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    plus_two,
+    reverse,
+    block,
+    plus_four,
+    colour
+}
