17d5b35c3f9173f1147d7bd28c0807890343b8a0
andol <andolo7.5@gmail.com>
2023-02-04 13:00:07 +0100

    defined get game state

--- server/src/router/uno.ts
+++ server/src/router/uno.ts
@@ -1,22 +1,11 @@
 import express from "express";
 import { Request, Response } from "express";
-import { Card } from "../model/card";
-
-class GameState {
-    yourPile: Array<Card>;
-    yourTurn: Boolean;
-    sizeGamePile: number;
-    sizeDrawPile: number;
-    sizeOppPile: number;
-    topCard: number;
-
-    constructor(yourPile : Array<Card>, yourTurn : Boolean, sizeGamePile : number, sizeDrawPile : number, sizeOppPile : number, topCard : number) {
-        this.yourPile = yourPile;
-        this.yourTurn = 
-    }
-}
+import { instantiateUnoService } from "../service/uno";
+import { GameState } from "../service/uno";
 
 export const unoRouter = express.Router();
+
+const unoService = instantiateUnoService();
 
 unoRouter.get("/", async (req: Request, res: Response) => {    
     res.status(200).send("It works!");
@@ -25,11 +14,11 @@ unoRouter.get("/", async (req: Request, res: Response) => {
 
 unoRouter.get("/game_state", async (
     req: Request,
-    res: Response<Array<Card>, Boolean, number >
+    res: Response<GameState>
 ) => {
     try {
-        const tasks = await unoService.getState();
-        res.status(200).send(state);
+        const GameState = await unoService.getState();
+        res.status(200).send(GameState);
     } catch (e: any) {
         res.status(500).send(e.message);
     }
