54c360241f1ff3ea90e450dfb8d3daa6e99a63b0
andol <andolo7.5@gmail.com>
2023-02-11 17:41:57 +0100

    more http requests handled

--- server/src/router/uno.ts
+++ server/src/router/uno.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { Request, Response } from "express";
+import { Card } from "../model/card";
+import { Colour } from "../model/Colour";
 import { instantiateUnoService } from "../service/Game";
 import { GameState } from "../service/GameState";
 
@@ -12,16 +14,78 @@ unoRouter.get("/", async (req: Request, res: Response) => {
 });
 
 
-unoRouter.get("/game_state", async (
+unoRouter.get("/uno/game_state", async (
     req: Request,
     res: Response<GameState>
 ) => {
     try {
-        const GameState = await unoService.getState(1);
-        res.status(200).send(GameState);
+        //const GameState = await unoService.getState();
+        //res.status(200).send(GameState);
     } catch (e: any) {
         res.status(500).send(e.message);
     }
 });
 
+unoRouter.post("/uno/select_card", async (
+    req: Request<{player_name : string , card : Card}>,
+    res: Response<Card | string>
+) => {
+    try {
+        const player = req.body.player_name;
+        const card = req.body.card;
+        if (typeof(player) !== "string") {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- player_name has type ${typeof(player)}`);
+            return;
+        }
+        if (typeof(card) !== typeof(Card)) {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- card has type ${typeof(card)}`);
+            return;
+        }
+        //unoService.putCard(player, card);
+
+    } catch (e: any) {
+        res.status(500).send(e.message)
+    }
+});
+
+unoRouter.post("/uno/select_color", async (
+    req: Request<{player_name : string , colour : Colour}>,
+    res: Response<Colour | string>
+) => {
+    try {
+        const player = req.body.player_name;
+        const colour = req.body.colour;
+        if (typeof(player) !== "string") {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- player_name has type ${typeof(player)}`);
+            return;
+        }
+        if (typeof(colour) !== typeof(Colour)) {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- colour has type ${typeof(colour)}`);
+            return;
+        }
+        //unoService.selectColor(player, colour);
+
+    } catch (e: any) {
+        res.status(500).send(e.message)
+    }
+});
+
+unoRouter.post("/uno/say_uno", async (
+    req: Request<{player_name : string}>,
+    res: Response<string>
+) => {
+    try {
+        const player = req.body.player_name;
+        if (typeof(player) !== "string") {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- player_name has type ${typeof(player)}`);
+            return;
+        }
+        //unoService.sayUno(player);
+
+    } catch (e: any) {
+        res.status(500).send(e.message)
+    }
+});
+
+// We want the clients to get specific information based on who is asking
 
