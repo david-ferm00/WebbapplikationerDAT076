720f70a77c0157d6962c324930d719381145486b
Anders <andeolof@student.chalmers.se>
2023-02-15 16:14:05 +0100

    added node packages

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { unoRouter } from "./router/uno";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(unoRouter);
