ec22799f79358764c9d346c43515d03cdc8f0110
david-ferm00 <david@ferm.co.uk>
2023-03-03 11:14:39 +0100

    linked the two pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,33 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
+import UnoGame from './UnoGame';
+import Mainpage from './Mainpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Mainpage />}/>
+        <Route path='/UnoGame' element={<UnoGame />}/>
+      </Routes>
+    </Router>
 
-        <p>
-          Bootstrap button 
-          <Button>
-            button!!!
-          </Button>
-        </p>
-
-      </header>
-    </div>
+    
   );
 }
 
--- server/src/router/matchmaking.ts
+++ server/src/router/matchmaking.ts
@@ -5,25 +5,32 @@ import { IUnoService } from "../service/Game";
 
 export const matchmakingRouter = express.Router();
 
-var unoService : IUnoService;
+var unoService : IUnoService = instantiateUnoService("-1","hello");
 
 //list of games
 matchmakingRouter.get("/matchmaking", async (req: Request, res: Response) => {    
     res.status(200).send("It works from matchmaking router!");
 });
 
+interface GamelistItem{
+    code : string,
+    noOfPlayers : number
+}
+
 matchmakingRouter.get("/matchmaking/gamelist", async(req: Request, res: Response) => {
-    if(unoService == undefined || unoService == null){
+    /*if(unoService == undefined || unoService == null){
         res.status(400).send(null)
     } else {
         res.status(200).send(unoService);
-    }
+    }*/
+    const gamelist : GamelistItem = {code : unoService.getCode()===undefined ? "" : unoService.getCode(), noOfPlayers : unoService.getNoOfPlayers()}
+    res.status(200).send(gamelist)
 });
 
 //create a game
 matchmakingRouter.post("/matchmaking/creategame/:code/:id", async(req : Request, res: Response) => {
     unoService = instantiateUnoService(req.params.code, req.params.id);
-
+    res.status(200)
     /*if(game code already exists){
         res.status(400).send("game code already exists");
     } else {
