1be6856f4150f477421d8a73dc2b89402ccb0738
Anders <andeolof@student.chalmers.se>
2023-03-13 16:58:42 +0100

    service testing

--- server/service.test.ts
+++ server/service.test.ts
@@ -4,11 +4,38 @@ test("When asking for gamestate before anything has happened the game state shou
     const GameManager = instantiateUnoService();
     GameManager.createGame("game_code","player_name");
     const gameState = GameManager.getState("player_name", "game_code");
-    expect(gameState.sizeDrawPile ==  81).toBeTruthy(); // total number of cards minus the amount dealt to players and game pile
-    expect(gameState.sizeGamePile == 1).toBeTruthy(); 
-    expect(gameState.topCard != undefined).toBeTruthy();
-    expect(gameState.yourPile.size() == 5).toBeTruthy();
-    expect(gameState.sizeOppPile == 5).toBeTruthy();
+    expect(gameState.sizeDrawPile ===  81) // total number of cards minus the amount dealt to players and game pile
+    expect(gameState.sizeGamePile == 1)
+    expect(gameState.topCard != undefined)
+    expect(gameState.yourPile.size() == 5)
+    expect(gameState.sizeOppPile == 5)
 })
 
-test("")
+test("Create new games", async () => {
+    const GameManager = instantiateUnoService();
+    GameManager.createGame("1", "player_name");
+    GameManager.createGame("2", "player_name");
+    GameManager.createGame("3", "player_name");
+    GameManager.createGame("4", "player_name");
+
+    expect(GameManager.getGameList.length === 4)
+})
+
+test("Multiple games with the same name", async () => {
+    const GameManager = instantiateUnoService();
+    GameManager.createGame("1", "player_name");
+    GameManager.createGame("2", "player_name");
+    GameManager.createGame("2", "player_name");
+
+    expect(GameManager.getGameList.length === 2)
+})
+
+test("Multiple games with the same name", async () => {
+    const GameManager = instantiateUnoService();
+    GameManager.createGame("1", "player_name");
+    GameManager.createGame("2", "player_name");
+    GameManager.createGame("2", "player_name");
+
+    expect(GameManager.getGameList.length === 3)
+})
+
