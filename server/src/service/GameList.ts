b15ebc7386d3aea345cee223a58cc1ee77327190
Anders <andeolof@student.chalmers.se>
2023-03-13 17:23:16 +0100

    .

--- server/src/service/GameList.ts
+++
@@ -1,9 +0,0 @@
-export class GameList{
-    gameCode : string;
-    noOfPlayers : number;
-
-    constructor(code : string, noOfPlayers : number){
-        this.gameCode = code;
-        this.noOfPlayers = noOfPlayers;
-    }
-}
